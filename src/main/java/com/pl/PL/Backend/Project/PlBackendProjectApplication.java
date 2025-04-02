10625da0f9fe66f943586c7e36c90b320068cc26
PSBrar <prabhjot.brarrr@gmail.com>
2025-04-02 16:27:47 +0100

    uploaded necessary files

---
+++ src/main/java/com/pl/PL/Backend/Project/PlBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pl.PL.Backend.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/PL/Backend/Project/PlBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.PL.Backend.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
